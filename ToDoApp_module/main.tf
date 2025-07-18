5e7c9b5af3957e77282bacd4b541be8376763832
Priya Patel <Swara13dec1997@gmail.com>
2025-07-18 18:29:34 +0530

    added 1rg

--- ToDoApp_module/main.tf
+++ ToDoApp_module/main.tf
@@ -8,8 +8,9 @@ module "rgmodule_11" {
     source = "../azurerm_child_module/resource_group"
     rg_name11 = "18julyfortesting"
     rg_location11 = "australia east"
-  
+ 
 }
+
 
 module "azurerm_virtual_network" {
 source = "../azurerm_child_module/Virtual_network_group"
